4de6fbfcfe542db65dbac412cae945204236e518
abimbola7 <abimbolababajide6@gmail.com>
2023-02-13 11:43:10 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 import Form from "./components/form";
 
 function App() {
-  const expenses = [
+  const [expenses, setExpensess] =  useState([
     {
       title : "Car insurance",
       amount : 294.67,
@@ -25,14 +26,36 @@ function App() {
       amount : 294.67,
       date : new Date(2023, 1, 3)
     },
-  ]
+  ])
   const getData = (data) => {
     console.log(data); 
   }
+
+
+
+  const [arr, newArr] = useState([
+    {
+      name: 'Abimbola',
+      school: 'OAU',
+      age: 22
+    }
+  ]);
+  // const newerOne = {};
+
+  // const pushh = () => {
+  //   newArr([...arr, newerOne]) 
+  //   console.log(arr)
+  // }
+
+
   const addExpenseHandler = expense => {
     console.log("In app js");
-    console.log(expense);
+    // console.log(expense);  
+    // console.log(expenses);
+    console.log(expenses);
+    // setExpensess([...expenses, expense]);
   }
+
   return(
     <div>
       <NewExpense onAddExpense={addExpenseHandler}/>
@@ -41,6 +64,12 @@ function App() {
       </Expenses>
       <Form
       onSubmit={getData}/>
+      <div>
+        <h1>{arr}</h1>
+        <button>
+          Push
+        </button>
+      </div>
     </div>
   )
 }
